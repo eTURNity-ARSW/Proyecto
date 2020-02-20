dadbda6328d8f1e8afd10227059c1080a944bfb1
unknown <jimmy.chirivi@labinfo.is.escuelaing.edu.co>
2020-02-20 15:59:43 -0500

    1

---
+++ src/test/java/arsw/proyecto/eTURNity/ETurNityApplicationTests.java
@@ -0,0 +1,13 @@
+package arsw.proyecto.eTURNity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ETurNityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
