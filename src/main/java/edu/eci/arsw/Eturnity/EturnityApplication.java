55bf666bfc299c8100da3b7f7f03574e91b40f8a
julianagarzond <juliana.garzon-d@mail.escuelaing.edu.co>
2020-03-09 16:48:03 -0500

    Funciona el index

--- src/main/java/edu/eci/arsw/Eturnity/EturnityApplication.java
+++ src/main/java/edu/eci/arsw/Eturnity/EturnityApplication.java
@@ -4,11 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-@EnableJpaRepositories("edu.eci.arsw.Eturnity.Repositories")
 @ComponentScan(basePackages= {"com.modeler"})
 @ComponentScan(basePackages = {"Eturnity"})
 public class EturnityApplication {
--- src/main/java/edu/eci/arsw/Eturnity/Persistence/UserPersistence.java
+++ src/main/java/edu/eci/arsw/Eturnity/Persistence/UserPersistence.java
@@ -1,9 +1,6 @@
 package edu.eci.arsw.Eturnity.Persistence;
 
 import edu.eci.arsw.Eturnity.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
