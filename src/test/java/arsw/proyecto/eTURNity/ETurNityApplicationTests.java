3efa0a929d38044b70ec78f68a6ac2f02efcdcdf
julianagarzond <juliana.garzon-d@mail.escuelaing.edu.co>
2020-03-09 15:56:43 -0500

    index

--- src/test/java/arsw/proyecto/eTURNity/ETurNityApplicationTests.java
+++ src/test/java/arsw/proyecto/eTURNity/ETurNityApplicationTests.java
@@ -1,7 +1,7 @@
 package arsw.proyecto.eTURNity;
 
 import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest;
 
 //@SpringBootTest
 class ETurNityApplicationTests {
