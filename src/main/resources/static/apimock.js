0419dea4224c81fce837992f89d4ebf929c97683
unknown <sarah.vieda@labinfo.is.escuelaing.edu.co>
2020-03-11 16:52:34 -0500

    user

---
+++ src/main/resources/static/apimock.js
@@ -0,0 +1,13 @@
+var apimock = (function () {
+    mockdata["sarah"]=[{"username":"sarah","password":"1234"}];
+
+    return{
+        getUser:function(username,password,callback){
+        callback(
+            mockdata[username,password]
+          );
+        }
+    };
+    }
+
+})();
