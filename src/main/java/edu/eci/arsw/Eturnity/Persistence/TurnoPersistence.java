3d063c62abd03cc7df6d86f5fd34786da13f65cd
Juancho García <juan.garcia-ga@mail.escuelaing.edu.co>
2020-03-16 13:08:37 -0500

    Solución Despliegue TurnoPersistence

--- src/main/java/edu/eci/arsw/Eturnity/Persistence/TurnoPersistence.java
+++ src/main/java/edu/eci/arsw/Eturnity/Persistence/TurnoPersistence.java
@@ -1,7 +1,9 @@
 package edu.eci.arsw.Eturnity.Persistence;
 import java.util.Set;
 import edu.eci.arsw.Eturnity.model.Turno;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface TurnoPersistence {
    public void guardarTurno(Turno t) throws TurnoException;
    public Turno getTurno(String id, String username) throws TurnoException;
