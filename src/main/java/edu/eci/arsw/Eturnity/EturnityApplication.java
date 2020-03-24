60bf12a0d28e5ebb22fc74dfe8f32bd6dd3aa887
camilavieda04 <camila_vieda04@hotmail.com>
2020-03-23 19:05:23 -0500

    app

--- src/main/java/edu/eci/arsw/Eturnity/EturnityApplication.java
+++ src/main/java/edu/eci/arsw/Eturnity/EturnityApplication.java
@@ -12,18 +12,15 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import java.util.Date;
-@SpringBootApplication
-/*@EnableAutoConfiguration*/
-@ComponentScan(basePackages = {"edu.eci.arsw.Eturnity.services", "edu.eci.arsw.Eturnity.controllers"})
-@EnableJpaRepositories("edu.eci.arsw.Eturnity.repositories")
-@EntityScan("edu.eci.Eturnity.model")
 
+import java.util.Date;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan(basePackages = {"edu.eci.arsw.Eturnity"})
 public class EturnityApplication implements CommandLineRunner {
 	private static final Logger log = LoggerFactory.getLogger(EturnityApplication.class);
 	@Autowired
