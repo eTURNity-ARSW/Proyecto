ab7376b529eba6e19867c375838991d4cedab384
julianagarzond <juliana.garzon-d@mail.escuelaing.edu.co>
2020-04-29 22:10:43 -0500

    apiclient

--- src/main/resources/static/js/apicli.js
+++ src/main/resources/static/js/apicli.js
@@ -3,15 +3,52 @@
 apicli = (function() {
 
     return {
-        getUsuarios: function(author, callback) {
+        getUsuarios: function(usuarios, callback) {
             jQuery.get({
                 dataType: "json",
-                url: "/usuarios",
+                url: "/users",
                 success: function (data) {
                     callback(data)
                 }
             });
         },
+
+        login: function(usuario,password,,callback){
+            jQuery.get({
+                        dataType: "json",
+                        url: "/login",
+                        success: function (data) {
+                            callback(data)
+                        }
+                    });
+
+        },
+
+        getUserByUsername: function(username,callback){
+                jQuery.get({
+                                    dataType: "json",
+                                    url: "/users/" +username,
+                                    success: function (data) {
+                                        callback(data)
+                                    }
+                                });
+
+            },
+
+        getUserByTurnos: function(turnos,callback){
+          jQuery.get({
+                                            dataType: "json",
+                                            url: "/users/" +turnos,
+                                            success: function (data) {
+                                                callback(data)
+                                            }
+                                        });
+
+                    },
+
+        }
+
+
         getTurnos: function( turnos, callback) {
 
             jQuery.get({
@@ -23,5 +60,38 @@ apicli = (function() {
             });
 
         },
+
+       getTurnsByUser: function(username,callback){
+
+         jQuery.get({
+                       dataType: "json",
+                       url: "/turnos/" +username,
+                       success: function (data) {
+                           callback(data)
+                       }
+                   });
+
+       },
+
+       getTurnsByDate :function(date,callback){
+        jQuery.get({
+                              dataType: "json",
+                              url: "/turnos/" +date,
+                              success: function (data) {
+                                  callback(data)
+                              }
+                          });
+       }
+
+       getTurnsBySede: function(sede,callback){
+            jQuery.get({
+                                     dataType: "json",
+                                     url: "/turnos/" +sede,
+                                     success: function (data) {
+                                         callback(data)
+                                     }
+                                 });
+              }
+       }
 
 }})();
